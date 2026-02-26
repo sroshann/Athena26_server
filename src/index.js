c31a6855558523910d70847f3440c19c45f3bc01
Shamil Roshan.N <shamilroshan390@gmail.com>
2026-02-27 01:02:27 +0530

    Basic configuration completed

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import { createServer } from 'http'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+const app = express()
+const server = createServer( app )
+dotenv.config()
+
+app.use( express.json() )
+app.use( cookieParser() )
+app.use( cors({
+
+    origin : 'http://localhost:5173' || 'https://athenatkmce.site',
+    credentials : true
+
+}) )
+
+server.listen( process.env.PORT || 5000, () => {
+
+    console.log('Server running')
+
+} )
+
